e5cf5dc4a8eac5543ff858fc94f83979e6fe385d
TerjeRusska <terjerusska@gmail.com>
2022-06-05 20:56:55 +0300

    generate table csv files

--- drop_table_per_item.py
+++ drop_table_per_item.py
@@ -1,9 +1,18 @@
 from osrsbox import monsters_api, items_api
 import json
+import csv
 import os
 
 drop_dict = dict()
 drop_hashes = list()
+
+items_table_header = ["id", "name", "is_members", "lowalch", "highalch", "examine"]
+items_table_rows = []
+monsters_table_header = ["id", "name", "combat_level", "wiki_url"]
+monsters_table_rows = []
+drops_table_header = ["id", "item_id", "monster_id", "is_members", "quantity", "is_noted", "rarity"]
+drops_table_rows = []
+drops_table_counter = 0
 
 if __name__ == '__main__':
     all_db_monsters = monsters_api.load()
@@ -12,6 +21,7 @@ if __name__ == '__main__':
     for monster in all_db_monsters:
 
         if monster.drops and not monster.duplicate:
+            monsters_table_rows.append([monster.id, monster.name, monster.combat_level, monster.wiki_url])
             for drop in monster.drops:
                 drop_hash = hash((monster.name, monster.combat_level,
                                   drop.members, drop.quantity, drop.noted, drop.rarity))
@@ -28,7 +38,10 @@ if __name__ == '__main__':
                         "examine": item.examine,
                         "drops": []
                     }
+                    print(item_info)
                     drop_dict[drop.id] = item_info
+                    items_table_rows.append(
+                        [drop.id, item.name, item.members, item.lowalch, item.highalch, item.examine])
                 drop_info = {
                     "id": monster.id,
                     "name": monster.name,
@@ -40,8 +53,26 @@ if __name__ == '__main__':
                     "rarity": drop.rarity
                 }
                 drop_dict[drop.id]["drops"].append(drop_info)
+                drops_table_rows.append(
+                    [drops_table_counter, drop.id, monster.id, drop.members, drop.quantity, drop.noted, drop.rarity])
+                drops_table_counter += 1
 
     path = 'output'
-    os.makedirs(path, exist_ok=False)
+    os.makedirs(path, exist_ok=True)
     with open("output/drop_table_per_item.json", "w") as outfile:
         json.dump(drop_dict, outfile)
+
+    with open("output/items_table.csv", "w", newline='') as items_table_csv:
+        writer = csv.writer(items_table_csv, quoting=csv.QUOTE_NONNUMERIC)
+        writer.writerow(items_table_header)
+        writer.writerows(items_table_rows)
+
+    with open("output/monsters_table.csv", "w", newline='') as monsters_table_csv:
+        writer = csv.writer(monsters_table_csv, quoting=csv.QUOTE_NONNUMERIC)
+        writer.writerow(monsters_table_header)
+        writer.writerows(monsters_table_rows)
+
+    with open("output/drops_table.csv", "w", newline='') as drops_table_csv:
+        writer = csv.writer(drops_table_csv, quoting=csv.QUOTE_NONNUMERIC)
+        writer.writerow(drops_table_header)
+        writer.writerows(drops_table_rows)
